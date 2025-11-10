2bc7855a7534337908518c79745ea0a32ea40d84
Gerardo Mena <gmena82@users.noreply.github.com>
2025-11-10 11:20:24 -0600

    Remove email from resume - direct users to contact form

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -179,15 +179,6 @@ function Resume() {
                     clipRule="evenodd"
                   ></path>
                 )
-              },
-              {
-                label: 'Gerardomena82@live.com',
-                icon: (
-                  <>
-                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
-                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
-                  </>
-                )
               }
             ].map((contact, index) => (
               <motion.div
